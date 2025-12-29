99d4149532ff11764442efd9a72e5ba5c1636d28
vanshagarwal20 <vanshagarwal3329@gmail.com>
2025-12-29 10:44:39 +0530

    [vansh] Add . scenarioBased

---
+++ core-java-practice/scenario-based/FestivalLuckyDraw.java
@@ -0,0 +1,30 @@
+package scenario_based;
+
+import java.util.*;
+
+public class FestivalLuckyDraw {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of visitors: ");
+        int visitors = sc.nextInt();
+
+        for (int i = 1; i <= visitors; i++) {
+            System.out.print("Visitor " + i + " - Enter your lucky number: ");
+            int number = sc.nextInt();
+
+            if (number <= 0) {
+                System.out.println("Invalid number. Try again.");
+                continue;
+            }
+
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println("Congratulations! You won a gift 🎁");
+            } else {
+                System.out.println("Sorry, better luck next time.");
+            }
+        }
+	}
+
+}
