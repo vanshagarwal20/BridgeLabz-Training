682a46fa30770171075044f9f214a6f666243742
vanshagarwal20 <vanshagarwal3329@gmail.com>
2026-01-19 16:04:58 +0530

    [vansh] Add . banking system problem

---
+++ reviews/BankingSystem/InsufficientBalanceException.java
@@ -0,0 +1,9 @@
+package reviews.BankingSystem;
+
+public class InsufficientBalanceException extends Exception{
+	
+	public InsufficientBalanceException(String message){		
+		super(message);	
+	}
+
+}
